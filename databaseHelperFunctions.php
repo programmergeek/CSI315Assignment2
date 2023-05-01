f33b8fa4ce45094684fe731a7f74ce6a3dff10d6
Will <programergeek@gmail.com>
2023-05-01 10:46:55 +0200

    feat: create function to connect to database

--- databaseHelperFunctions.php
+++ databaseHelperFunctions.php
@@ -1,4 +1,19 @@
 <?php
+
+    function connectToDatabase(){
+        $dbName = "";
+		$serverName = "10.0.19.74";
+		$username = "";
+		$password = "";
+		$conn = new mysqli($serverName, $username, $password, $dbName);
+		if($conn->connect_error){
+			die("Connection Failed: " . $conn->connect_error);
+		}else{
+			echo "connection successful";
+            return $conn;
+		}
+    }
+
 /*
     Creates an insert SQL query
     parameters: $tableName: string, $columnNames: array of strings , $valuesCount: number of values
@@ -23,4 +38,22 @@
         }
         return "INSERT INTO " . $tableName . "(" . $columns . ") " . "VALUES(" . $valuePlaceholders . ")";
     }
+
+    // function createTables($conn){
+    //     $createTutorTable = "CREATE TABLE Tutors(
+    //         fullName VARCHAR(255) NOT NULL,
+    //         email VARCHAR(255) NOT NULL,
+    //         id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    //         gender VARCHAR(5) NOT NULL,
+    //         experience INT(5) NOT NULL,
+    //         subjects VARCHAR(50) NOT NULL
+    //     )";
+
+    //     $stm = $conn->prepare($createTutorTable);
+    //     if($stm->execute()){
+    //         echo "tutors table created";
+    //     }else{
+
+    //     }
+    // }
 ?>
